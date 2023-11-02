9d1a8f30efd656810ec78360710afc11f6da2fbe
iOS-Senapi <pradeep.iossenpai@gmail.com>
2023-11-02 13:03:04 +0530

    Initial commit

---
+++ CICDAutomationDemo/CICDAutomationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDAutomationDemoApp.swift
+//  CICDAutomationDemo
+//
+//  Created by Pradeep's MacBook on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CICDAutomationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
